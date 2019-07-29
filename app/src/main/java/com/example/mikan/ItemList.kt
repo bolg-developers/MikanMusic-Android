44c53da42daa64b61cacbbbf9cc4ca1193a8af75
deodorantpower <yuta.0401.hsgw@gmail.com>
2019-07-30 06:59:42 +0900

    ItemList.ktの無駄コードの削除

--- app/src/main/java/com/example/mikan/ItemList.kt
+++ app/src/main/java/com/example/mikan/ItemList.kt
@@ -8,15 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import com.example.mikan.await.HttpUtil
 import kotlinx.android.synthetic.main.activity_itemlist.*
 import kotlinx.android.synthetic.main.list_item.view.*
-
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import org.json.JSONObject
 
 class ItemList : AppCompatActivity() {
 
@@ -29,33 +22,12 @@ class ItemList : AppCompatActivity() {
         "プレイリスト"
     )
 
-    //val dbhelper = DBHelper(this)
-    //var data:String? = null
-
     // List生成と初期化
     val itemdata = List(titletext.size) { i -> ItemListData(titletext[i], itemicon[i])}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_itemlist)
-
-        // progressbar設定
-        //progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        //progressBar.visibility = View.INVISIBLE
-
-       /* var cur = dbhelper.GetAll()
-        if (cur.moveToFirst()) {
-            do {
-                data += "DisplayNameは"
-                data += "\t"
-                data += cur.getString(2)
-                data += "です\n"
-                Log.d("hs/db", "データの内容$data")
-            } while (cur.moveToNext())
-        }
-        cur.close()
-
-        dbhelper.closeDB()*/
 
         val adapter = ItemListAdapter(this, itemdata)
         listView.adapter = adapter
@@ -115,27 +87,4 @@ class ItemList : AppCompatActivity() {
      * */
     data class ViewHolder(val titleTextView: TextView, val itemImgView: ImageView)
 
-
-    /**
-     * gfunc
-     * 音楽の全ジャンルを取得するタスク
-     * */
-    fun gfunc() = GlobalScope.launch(Dispatchers.Main){
-        val ag = HttpUtil()
-        var str:String ? = null
-
-        // ここで非同期
-        async (Dispatchers.Default){ ag.genres(R.string.baseurl.toString() + "v1/genres")}.await().let {
-            // jsonを格納
-            val result = JSONObject(it)
-            val genres = result.getJSONArray("genres")
-
-            for (item in 0..(genres.length()-1)) {
-                // インデックスで情報を指定する
-                val randb = genres.getJSONObject(item)
-                str += randb.getString("id") + randb.getString("name") + "\n"
-            }
-            //progressBar.visibility = View.GONE
-        }
-    }
 }
--- app/src/main/java/com/example/mikan/ItemListData.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mikan
-
-object ItemListData {
-
-}
